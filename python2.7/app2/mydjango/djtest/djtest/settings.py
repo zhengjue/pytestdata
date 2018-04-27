8233dbde019155f0b1e158318f7bd0d3d38d6bcf
LZA <318724186>
2018-04-27 17:17:41 +0800

    2018-04-27

--- python2.7/app2/mydjango/djtest/djtest/settings.py
+++ python2.7/app2/mydjango/djtest/djtest/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         # 'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'DIRS': [os.path.join(BASE_DIR,"common/templates")],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +120,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    "common/image",
-]
